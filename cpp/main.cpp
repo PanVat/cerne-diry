0fb14fc182b5afd9fc48a236d56013db633f50a9
PanVat <panvatmail@gmail.com>
2025-05-15 14:00:45 +0200

    Opravení chyb, validace formuláře a začátek back-endu

---
+++ cpp/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Content-Type: text/html\n\n";
+    std::cout << "Hello to Apache and Firefox!\n";
+    return 0;
+}
